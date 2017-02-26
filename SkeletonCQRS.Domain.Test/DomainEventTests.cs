3d608edb737af8ccae2ed0ef29e7fe4ce92f10ad
william <william.mcnulty@live.co.uk>
2017-02-26 15:17:59 +0000

    Added Domain test project

---
+++ SkeletonCQRS.Domain.Test/DomainEventTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using SkeletonCQRS.Domain.Aggregates;
+using SkeletonCQRS.Domain.Events;
+using SkeletonCQRS.Infrastructure.Events;
+
+namespace SkeletonCQRS.Domain.Test
+{
+    [TestFixture]
+    public class DomainEventTests
+    {
+        private static IEnumerable<Type> AllDomainEventTypes()
+        {
+            var domainEventInterfaceType = typeof(DomainEvent);
+            var domainEventTypes = Assembly.GetAssembly(typeof(InventoryItem)).GetTypes()
+                .Where(x => domainEventInterfaceType.IsAssignableFrom(x));
+            return domainEventTypes;
+        }
+
+        [Test]
+        public void AllDomainEventsCanBeSerializedAndDeserializedCorrectly()
+        {
+            var serializer = new EventXmlSerializer();
+            var domainEventTypes = AllDomainEventTypes();
+
+            foreach (var domainEventType in domainEventTypes)
+            {
+                var domainInstance = Activator.CreateInstance(domainEventType, true) as IDomainEvent;
+                var serializedInstance = serializer.Serialize(domainInstance);
+                var deserializedInstance = serializer.Deserialize(serializedInstance, domainEventType);
+                Assert.AreEqual(domainInstance, deserializedInstance);
+                Console.WriteLine("{0} serialized and deserialized correctly.", domainEventType);
+            }
+        }
+
+        [Test]
+        public void AllDomainEventsAreImmutable()
+        {
+            var domainEventTypes = AllDomainEventTypes();
+
+            foreach (var domainEventType in domainEventTypes)
+            {
+                var constructors = domainEventType.GetConstructors();
+                Assert.That(constructors.Length, Is.EqualTo(1));
+            }
+        }
+    }
+}
--- SkeletonCQRS.Infrastructure/Events/DomainEvent.cs
+++ SkeletonCQRS.Infrastructure/Events/DomainEvent.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
@@ -24,13 +27,59 @@ namespace SkeletonCQRS.Infrastructure.Events
 
         [Obsolete]
         [EditorBrowsable(EditorBrowsableState.Never)]
-        protected DomainEvent() { }
+        protected DomainEvent()
+        {
+        }
+
         protected DomainEvent(Guid aggregateId, int aggregateVersion)
         {
             AggregateId = aggregateId;
             AggregateVersion = aggregateVersion;
             EventId = Guid.NewGuid();
             TimeStamp = DateTime.UtcNow;
+        }
+
+        protected bool Equals(DomainEvent other)
+        {
+            foreach (var equalityProperty in EqualityProperties())
+            {
+                var myValue = equalityProperty.GetValue(this);
+                var theirValue = equalityProperty.GetValue(other);
+                if (!Equals(myValue, theirValue))
+                    return false;
+            }
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((DomainEvent)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashcode = 0;
+                var counter = 0;
+                foreach (var equalityProperty in EqualityProperties())
+                {
+                    if (counter == 0)
+                        hashcode = equalityProperty.GetValue(this)?.GetHashCode() ?? 0;
+                    else
+                        hashcode = (hashcode * 397) ^ equalityProperty.GetValue(this)?.GetHashCode() ?? 0;
+                    counter++;
+                }
+                return hashcode;
+            }
+        }
+
+        private IEnumerable<PropertyInfo> EqualityProperties()
+        {
+            return GetType().GetProperties();
         }
     }
 }
